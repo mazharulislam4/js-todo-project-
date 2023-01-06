0b0247270f32e08e01a3382a71008bc3999c3bdf
Mazharul Islam Tamim <mazharulislamtamim28@gmail.com>
2023-01-07 01:39:42 +0600

    updated

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 const todoNameEle = document.querySelector('#name');
 const todoDescriptionEle = document.querySelector('#description');
-const tableBody = document.querySelector('table tbody');
+const todo = document.querySelector('.todo');
 
 
-
-function submitHandler(e){
-  e.preventDefault();
-
-let todoName = todoNameEle.value;
-let todoDescription = todoDescriptionEle.value;
-
-
-render({todoName: todoName , todoDescription: todoDescription});
-
-}
 
 const date = new Date();
 
@@ -35,33 +24,98 @@ const time = `${hours} : ${minutes} ${amPm}`;
 const formatedDate = convertedArry.slice(1 , 4 ).join(' ');
 
 
+
+// submitHandler
+
+
+function submitHandler(e){
+  e.preventDefault();
+
+let todoName = todoNameEle.value;
+let todoDescription = todoDescriptionEle.value;
+
+
+render({todoName: todoName , todoDescription: todoDescription});
+
+}
+
+// html rendering
+
 function render(formData){
-  tableBody.innerHTML += `<tr>
-    <td>${formData.todoName}</td>
-    <td>${formData.todoDescription}</td>
-    <td>${formatedDate}</td>
-    <td>${time}</td>
-  <td>
-  <button type="button"  name="button" class= "deleteBtn" >Delete</button>
-  </td>
-  </tr>`;
 
-  todoNameEle.value = '';
-  todoDescriptionEle.value = '' ;
+if(!document.querySelector('.todo table')){
+  todo.innerHTML = `<table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
 
+  <tbody>
 
-const deleteBtn  = document.querySelectorAll('.deleteBtn');
+  </tbody>
 
-
-for(let i = 0; i < deleteBtn.length; i++){
-deleteBtn[i].addEventListener('click' , function(e){
-removeEle(e.target.parentNode.parentNode)
-})
-}
+    </table>`;
 
 }
 
+
+const table = document.querySelector('.todo table');
+const tableBody = document.querySelector('table tbody');
+
+table.style.visibility = "visible";
+
+const tr = document.createElement('tr') ;
+
+tr.innerHTML = `
+<td>${formData.todoName}</td>
+<td>${formData.todoDescription}</td>
+<td>${formatedDate}</td>
+<td>${time}</td>
+<td>
+<button type="button"  name="button" class= "deleteBtn" >Delete</button>
+</td>
+`;
+
+
+tableBody.insertAdjacentElement('beforeend' , tr)
+
+    todoNameEle.value = '';
+    todoDescriptionEle.value = '' ;
+
+
+  const deleteBtn  = document.querySelectorAll('.deleteBtn');
+
+
+  for(let i = 0; i < deleteBtn.length; i++){
+  deleteBtn[i].addEventListener('click' , function(e){
+  removeEle(e.target.parentNode.parentNode)
+  })
+  }
+
+
+}
+
+
+
+
+// remove table row 
 
 function removeEle(ele){
   ele.remove();
+
+const table = document.querySelector('.todo table');
+
+
+if(table.querySelector('tbody').children.length == 0){
+  table.style.visibility = "hidden";
+}else {
+  table.style.visibility = "visible";
+}
+
+
 }
